2ed526d7bf5aa78d2bc424d5b54ff1e8bed0be06
Amanda Perry <challenge641@gmail.com>
2022-09-02 16:37:22 -0500

    created homepage and npm start works

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# determined_mvc_blog
+# Bit Bites Tech Blog
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://choosealicense.com/licenses/mit/)
+
+## Description
+SocialMedia Application gathering point to post, discuss and promote all things tech. Once account is created, users are able to create posts and view from their own dashboard. Users may also make and read comments made by others as well as promote up-standing, interesting, and helpful information that benefits the community.
+
+## Table of Contents
+  - [Usage](#usage)
+  - [Tests](#tests)
+  - [Installation](#installation)
+  - [Screenshots and Video](#screenshots_video)
+  - [Contributing](#contributing)
+  - [Questions](#questions)
+
+  - [License](#license)
+      
+
+
+## Usage
+Users can post and see posts by others; tag topics of discussion; comment and upvote other's posts. This application uses node.js packages: express, express-handlebars, express-session, connect-session-sequelize, sequelize, mysql2, dotenv, and bcrypt
+
+
+
+## Tests
+TBD
+
+
+
+## Installation
+Install Node.js, Clone DB, in terminal npm init or npm init -y, then npm i <package name> for each 
+
+
+## Screenshots and Video
+coming soon
+![Alt Text](./images/screenshot.png)
+
+
+
+## Contributing
+coming soon
+
+
+
+## Questions
+
+Have questions?
+You can find me on GitHub:
+https://github.com/BeKind-Rewind
+
+Or email me at:
+Challenge641@gmail.com
+
+
+## License
+
+Licensed under the [MIT](https://choosealicense.com/licenses/mit/) license.
+    
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const routes = require('./controllers');
+// const routes = require('./controllers');
 // Utilizes cookies to save and retrieve data
-const session = require('express-session');
+// const session = require('express-session');
 const path = require('path');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 // Utilizing session storage
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,21 +15,21 @@ const PORT = process.env.PORT || 3001;
 // Set up Handlebars with custom helpers
 const hbs = exphbs.create({ helpers });
 
-const sess = {
-  secret: 'Super Secret Secret',
-  cookie: {},
-  resave: false,
-  // forces a session to be saved to the store
-  // false may be preferred here for implementing login sessions
-  saveUninitialized: true,
-  // note: default server-side session storage is purposely not designed for a production environment
-  // it will leak memory under most conditions
-  store: new SequelizeStore({
-      db: sequelize
-  })
-};
+// const sess = {
+//   secret: 'Super Secret Secret',
+//   cookie: {},
+//   resave: false,
+//   // forces a session to be saved to the store
+//   // false may be preferred here for implementing login sessions
+//   saveUninitialized: true,
+//   // note: default server-side session storage is purposely not designed for a production environment
+//   // it will leak memory under most conditions
+//   store: new SequelizeStore({
+//       db: sequelize
+//   })
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -38,7 +38,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on PORT: ${PORT}!`));
