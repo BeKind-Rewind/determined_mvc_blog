89bed6d4be369944cb2409638a9744a763c3d47e
Amanda Perry <challenge641@gmail.com>
2022-09-04 13:18:23 -0500

    added much of the front end HTML in handlebars

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+// sets up an Express session and connects the session to our Sequelize db
+const session = require('express-session');
+// set up Handlebars.js as app's template engine of choice
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+// importing the connection to sequelize from config/connection.js
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret', // replace with actual secret to store in .env
+    cookie: {}, // tells our session to use cookies
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+  
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening'));
 });
