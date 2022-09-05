4afe7b014f89bf832c83909a6f3dd0578a2c090c
Amanda Perry <challenge641@gmail.com>
2022-09-04 19:35:50 -0500

    moved signup to its own handlebars and js file

--- server.js
+++ server.js
@@ -2,21 +2,24 @@ const path = require('path');
 const express = require('express');
 // sets up an Express session and connects the session to our Sequelize db
 const session = require('express-session');
+// importing the connection to sequelize from config/connection.js
+const sequelize = require('./config/connection');
 // set up Handlebars.js as app's template engine of choice
 const exphbs = require('express-handlebars');
-
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-// importing the connection to sequelize from config/connection.js
-const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const sess = {
     secret: 'Super secret secret', // replace with actual secret to store in .env
-    cookie: {}, // tells our session to use cookies
-    resave: false,
+    cookie: {
+      // Session will automatically expire in 10 minutes
+      expires: 10 * 60 * 1000
+    }, 
+    resave: true,
+    rolling: true,
     saveUninitialized: true,
     store: new SequelizeStore({
       db: sequelize
@@ -24,20 +27,17 @@ const sess = {
 };
   
 app.use(session(sess));
-
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+// const routes = require('./controllers');
 app.use(require('./controllers/'));
+// app.use(routes);
 
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
