fc4e987331337358b9a73839fdb9653914053d2a
Amanda Perry <challenge641@gmail.com>
2022-09-06 15:12:34 -0500

    added create-post js and updated some routes and seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,54 +1,73 @@
-const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User } = require('../models');
 
 const userdata = [
   {
     username: 'alesmonde0',
+    twitter: 'alesmonde0',
+    github: 'alesmonde0',
     email: 'nwestnedge0@cbc.ca',
     password: 'password123'
   },
   {
     username: 'jwilloughway1',
+    twitter: 'jwilloughway1',
+    github: 'jwilloughway1',
     email: 'rmebes1@sogou.com',
     password: 'password123'
   },
   {
     username: 'iboddam2',
+    twitter: 'iboddam2',
+    github: 'iboddam2',
     email: 'cstoneman2@last.fm',
     password: 'password123'
   },
   {
     username: 'dstanmer3',
+    twitter: 'dstanmer3',
+    github: 'dstanmer3',
     email: 'ihellier3@goo.ne.jp',
     password: 'password123'
   },
   {
     username: 'djiri4',
+    twitter: 'djiri4',
+    github: 'djiri4',
     email: 'gmidgley4@weather.com',
     password: 'password123'
   },
   {
     username: 'msprague5',
+    twitter: 'msprague5',
+    github: 'msprague5',
     email: 'larnout5@imdb.com',
     password: 'password123'
   },
   {
     username: 'mpergens6',
+    twitter: 'mpergens6',
+    github: 'mpergens6',
     email: 'hnapleton6@feedburner.com',
     password: 'password123'
   },
   {
     username: 'tpenniell7',
+    twitter: 'tpenniell7',
+    github: 'tpenniell7',
     email: 'kperigo7@china.com.cn',
     password: 'password123'
   },
   {
     username: 'msabbins8',
+    twitter: 'msabbins8',
+    github: 'msabbins8',
     email: 'lmongain8@google.ru',
     password: 'password123'
   },
   {
     username: 'jmacarthur9',
+    twitter: 'jmacarthur9',
+    github: 'jmacarthur9',
     email: 'bsteen9@epa.gov',
     password: 'password123'
   }
